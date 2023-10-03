a2ef85f2f447adbc9e6013b0edcd290825e45be7
AlejandrVergara <josea.vergarar@ecci.edu.co>
2023-10-03 02:07:44 -0500

    w

--- R/e2.R
+++ R/e2.R
@@ -1,1 +1,14 @@
-r
+library(tidyverse)
+folder<-dirname(rstudioapi::getSourceEditorContext()$path)
+
+parentFolder <-dirname(folder)
+
+data <-
+  read.csv(paste0(parentFolder,"/Dataset/diabetes_012.csv"))
+
+summary(data)
+# Visualizaciones
+ggplot(data, aes(x = Age, y = BMI)) + geom_boxplot()
+
+# Binarización de la variable de clase
+data$Diabetes_012 <- ifelse(data$Diabetes_012 == 0, 0, 1)
